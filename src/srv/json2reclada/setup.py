80672a46f74ab77289ee83febeae11ed50c71fe2
Alexey Dobrov <alexey.dobrov@quantori.com>
2021-10-11 14:57:07 +0300

    author email fix

--- src/srv/json2reclada/setup.py
+++ src/srv/json2reclada/setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.0',
     description='Hierarchical JSON to Reclada objects converter',
     author='Alexei Dobrov',
-    author_email='alexey.dobrov@quantori.com',
+    author_email='adobrov@aiire.org',
     url='https://github.com/reclada/SciNLP/src/srv/json2reclada',
     packages=['json2reclada'],
     package_dir={'json2reclada': 'src'}
