ab70b6af29934fae696b988325e8fe713303e63d
AndreyRuma <andrey.rumyantsev@quantori.com>
2022-03-11 14:10:14 +0300

    Installation packages into reclada namespace

--- src/srv/bd2reclada/setup.py
+++ src/srv/bd2reclada/setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_namespace_packages
+import os
+
+def create_links(work_dir):
+    """
+        This function prepares the directory tree structure so
+        this package can be installed as a part of reclada namespace
+    :param work_dir: directory in which the directory tree structure would be created
+    """
+    # create reclada directory
+    os.mkdir(work_dir+"/reclada")
+    # create a symbolic link to src folder
+    os.symlink(work_dir+"/src", work_dir+"/reclada/bd22reclada", target_is_directory=True)
+
+# get the current directory
+cur_dir = os.getcwd()
+# create reclada directory and symlink json2reclada to src folder
+create_links(cur_dir)
 
 setup(
     name='bd2reclada',
@@ -9,6 +27,5 @@ setup(
     author='Alexei Dobrov',
     author_email='alexey.dobrov@quantori.com',
     url='https://github.com/reclada/SciNLP/src/srv/bd2reclada',
-    packages=['bd2reclada'],
-    package_dir={'bd2reclada': 'src'}
+    packages=find_namespace_packages(include=["reclada.*"]),
 )
--- src/srv/json2reclada/setup.py
+++ src/srv/json2reclada/setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
+from setuptools import find_namespace_packages 
+import os
+
+def create_links(work_dir):
+    """
+        This function prepares the directory tree structure so
+        this package can be installed as a part of reclada namespace
+    :param work_dir: directory in which the directory tree structure would be created
+    """
+    # create reclada directory
+    os.mkdir(work_dir+"/reclada")
+    # create a symbolic link to src folder
+    os.symlink(work_dir+"/src", work_dir+"/reclada/json2reclada", target_is_directory=True)
+
+# get the current directory
+cur_dir = os.getcwd()
+# create reclada directory and symlink json2reclada to src folder
+create_links(cur_dir)
 
 setup(
     name='json2reclada',
@@ -9,6 +26,5 @@ setup(
     author='Alexei Dobrov',
     author_email='alexey.dobrov@quantori.com',
     url='https://github.com/reclada/SciNLP/src/srv/json2reclada',
-    packages=['json2reclada'],
-    package_dir={'json2reclada': 'src'}
+    packages=find_namespace_packages(include=["reclada.*"]),
 )
